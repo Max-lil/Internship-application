809a7db0dff17d938fcc55acdf6c30060e3a1f0e
Max Liljestrand <max.liljestrand2@gmail.com>
2025-05-23 10:59:29 +0200

    Initial commit

---
+++ src/test/java/com/example/internshipapplication/InternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internshipapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
