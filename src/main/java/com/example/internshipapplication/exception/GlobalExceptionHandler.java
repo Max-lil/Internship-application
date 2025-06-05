edc3fb16126ac893f7ab8d5722fcbb42eaccf2bb
Loffe91 <olofsvensson91@gmail.com>
2025-06-05 12:44:57 +0200

    Felhantering och validering

---
+++ src/main/java/com/example/internshipapplication/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.internshipapplication.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidInputException.class)
+    public ResponseEntity<String> handleInvalidInput(InvalidInputException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ett internt serverfel uppstod.");
+    }
+
+}
