9bad06b1c700dbac021de3bb12ce81a6bf1d25ce
Loffe91 <olosve910707@student.jenseneducation.se>
2025-08-12 10:57:18 +0200

    Med test

--- src/main/java/com/example/internshipapplication/Student/Student.java
+++ src/main/java/com/example/internshipapplication/Student/Student.java
@@ -154,4 +154,6 @@ public class Student {
                 .map(Skill::getName)
                 .collect(Collectors.toSet());
     }
+
+
 }
--- src/main/java/com/example/internshipapplication/Student/StudentRepository.java
+++ src/main/java/com/example/internshipapplication/Student/StudentRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    Student findById(Integer id);
+
     // kolla dubbletter av e-post
     boolean existsByEmail(String email);
 }
